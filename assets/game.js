29f778769ea98eca4fdc8b2c10ff7266b98bd179
Christina Brenia <ChristinaBrenia@Christinas-MBP.home>
2020-11-07 17:08:25 -0500

    added javaScript - not finished

--- assets/game.js
+++ assets/game.js
@@ -11,3 +11,87 @@
     let questionCounter = 0 
     let availableQuestions = []
 
+//Questions array
+
+//Question 1
+let questions = [
+    {
+        question: 'What is 2+2?',
+        choice1= '2',
+        choice2= '4',
+        choice3= '21',
+        choice4= '6',
+        answer: 4,
+
+    },
+
+//Question 2
+
+    {
+        question: 'What is 5+2?',
+        choice1= '2',
+        choice2= '4',
+        choice3= '21',
+        choice4= '7',
+        answer: 2,
+
+    },
+//Question 3
+
+    {
+        question: 'What is 9+2?',
+        choice1= '2',
+        choice2= '4',
+        choice3= '21',
+        choice4= '11',
+        answer: 4,
+
+    },
+
+//Question 4
+
+    {
+        question: 'What is 6+2?',
+        choice1= '2',
+        choice2= '4',
+        choice3= '8',
+        choice4= '18',
+        answer: 3,
+
+    },
+//Question 5
+
+    {
+        question: 'What is 23+2?',
+        choice1= '25',
+        choice2= '4',
+        choice3= '24',
+        choice4= '6',
+        answer: 1,
+
+    }
+]
+
+// Fixed score and question
+const SCORE_POINTS = 100
+const MAX_Question = 4
+
+//Start game function - start score and question at 0 
+startGame = () => {
+    questionCounter =0
+    score = 0
+    //spread opperator to retrieve question value from array
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+//Function to get question
+getNewQuestion = () => {
+    if(availableQuestions.length ===0 || questionsCounter > MAX_Questions) {
+        localStorage.setItem('mostRecentScore',score)
+        return window.location.assign('/end.html')
+    }
+    //Calculates question on and correspond with percentage completed
+    questionCounter++
+    progressText.innerText = 'Question ${questionCounter} of ${MAX_QUESTIONS}'
+    progressBarFull.getElementsByClassName.width = '${ (questionCounter/MAX_QUESTIONS) *100}%'
+}
